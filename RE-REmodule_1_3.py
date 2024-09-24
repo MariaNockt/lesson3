961603a489a261e476daa7a45adb0aae3b4b18eb
MariaNockt <m.perevozchi04kova@gmail.com>
2024-09-24 17:17:31 +0400

    Add files via upload

---
+++ RE-REmodule_1_3.py
@@ -0,0 +1,8 @@
+name = 'Мария'
+print('Name:', name)
+age = 22
+print('Age:', age)
+age = age + 3 #аааааааааааа, тепрь поонятно (・–・;)ゞ
+print('New age:', age)
+is_student = True
+print('Is Student:', is_student)
